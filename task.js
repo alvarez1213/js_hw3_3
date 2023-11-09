7ce4f6cb10d40360e4c9bc0643c4b490c4ed8dcf
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2023-11-09 21:06:10 +0300

    modified task.js

---
+++ task.js
@@ -0,0 +1,30 @@
+const fontElements = document.querySelectorAll('.font-size')
+let fontActiveElement = document.querySelector('.font-size_active')
+
+const book = document.getElementById('book')
+
+for (const font of fontElements) {
+    font.addEventListener('click', (e) => {
+        if (font !== fontActiveElement) {
+            fontActiveElement.classList.remove('font-size_active')        
+            font.classList.add('font-size_active')
+            fontActiveElement = font
+            
+            if (font.getAttribute('data-size') === 'small') {
+                book.classList.add('book_fs-small')
+                book.classList.remove('book_fs-big')
+            }
+            if (font.getAttribute('data-size') === 'big') {
+                book.classList.add('book_fs-big')
+                book.classList.remove('book_fs-small')
+            }
+            if (font.getAttribute('data-size') === null) {
+                book.classList.remove('book_fs-big')
+                book.classList.remove('book_fs-small')
+            }
+        }
+        
+        // запрет перехода по ссылке
+        e.preventDefault()
+    })
+}
